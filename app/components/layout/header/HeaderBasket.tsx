c69324b655ffcc8f8454487d731a57bb1711592c
exigoll <exigoll@bk.ru>
2025-05-27 11:24:19 +0500

    fix

--- app/components/layout/header/HeaderBasket.tsx
+++ app/components/layout/header/HeaderBasket.tsx
@@ -6,19 +6,9 @@ import { ShoppingCartIcon } from "@/components/icons";
 
 import { useBasketStore } from "@/store/useBasketStore";
 
-import styles from "@/styles/components/layout/header/Header.module.css";
+import { pluralize } from "@/utils/pluralize";
 
-const pluralize = (
-  count: number,
-  [one, few, many]: [string, string, string]
-) =>
-  count % 10 === 1 && count % 100 !== 11
-    ? one
-    : count % 10 >= 2 &&
-        count % 10 <= 4 &&
-        (count % 100 < 10 || count % 100 >= 20)
-      ? few
-      : many;
+import styles from "@/styles/components/layout/header/Header.module.css";
 
 export const HeaderBasket = () => {
   const totalCount = useBasketStore(state => state.getTotalCount());
