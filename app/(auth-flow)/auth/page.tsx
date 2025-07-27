461580b1c6feeb0e31534a524f57f966700683a2
AndrewSkrynnik <skrinnik306603@gmail.com>
2025-07-27 11:33:37 +0500

    upd

--- app/(auth-flow)/auth/page.tsx
+++ app/(auth-flow)/auth/page.tsx
@@ -1,6 +1,9 @@
 import { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 import { AuthTemplate } from "@/features/auth/components/AuthTemplate";
+
+import { cookies } from "next/headers";
 
 export const metadata: Metadata = {
   title: "Вход и регистрация | rotazap.ru",
@@ -8,6 +11,13 @@ export const metadata: Metadata = {
     "Вход и регистрация в интернет-магазин автозапчастей rotazap.ru. Получите доступ к истории заказов, избранному и персональным предложениям."
 };
 
-export default function AuthPage() {
+export default async function AuthPage() {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken");
+
+  if (accessToken) {
+    redirect("/"); // если уже авторизован, редиректим
+  }
+
   return <AuthTemplate />;
 }
