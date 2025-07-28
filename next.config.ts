1181506e94c8a6d9575662e4129bca3b295c481b
AndrewSkrynnik <skrinnik306603@gmail.com>
2025-07-28 19:13:43 +0500

    upd

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,7 @@ const nextConfig: NextConfig = {
     ];
   },
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
