e444d53a84b0a9a10707934bfaa8f989b05d9b6c
AndrewSkrynnik <skrinnik306603@gmail.com>
2025-08-06 15:16:02 +0500

    upd

--- app/(auth-flow)/auth/page.tsx
+++ app/(auth-flow)/auth/page.tsx
@@ -10,7 +10,19 @@ export const metadata: Metadata = {
     title: "Вход и регистрация | rotazap.ru",
     description:
       "Вход и регистрация в интернет-магазин автозапчастей rotazap.ru. Получите доступ к истории заказов, избранному и персональным предложениям",
-    images: ["https://rotazap.ru/opengraph-image.png"]
+    url: "https://rotazap.ru/auth",
+    siteName: "rotazap.ru",
+    type: "website",
+    locale: "ru_RU",
+    images: [
+      {
+        url: "https://rotazap.ru/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "rotazap.ru – интернет-магазин автозапчастей",
+        type: "image/png"
+      }
+    ]
   },
   twitter: {
     card: "summary_large_image",
