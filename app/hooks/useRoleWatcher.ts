9ea20616e8eebf043c4e38cb38eb74da391a5e9c
AndrewSkrynnik <skrinnik306603@gmail.com>
2025-08-08 11:14:20 +0500

    upd: del console.log

--- app/hooks/useRoleWatcher.ts
+++ app/hooks/useRoleWatcher.ts
@@ -12,19 +12,19 @@ export function useRoleWatcher() {
 
   useEffect(() => {
     if (!user) {
-      console.log(
+      /* console.log(
         "Пользователь не авторизован, пропускаем подключение WebSocket."
-      );
+      ); */
       return; // Если пользователь не авторизован, ничего не делаем
     }
 
     if (!socket.connected) {
-      console.log("WebSocket не был подключен, подключаем...");
+      //console.log("WebSocket не был подключен, подключаем...");
       socket.connect();
     }
 
     socket.on("connect", () => {
-      console.log("WebSocket connected:", socket.id);
+      //console.log("WebSocket connected:", socket.id);
 
       // Отправляем событие для проверки роли
       socket.emit("checkRole", { userId: user.id });
@@ -32,7 +32,7 @@ export function useRoleWatcher() {
 
     // Слушаем событие обновления роли
     socket.on(`roleUpdate:${user.id}`, role => {
-      console.log("Role updated:", role);
+      //console.log("Role updated:", role);
 
       // Обновляем роль в состоянии Zustand
       setAuth({
@@ -42,15 +42,15 @@ export function useRoleWatcher() {
     });
 
     socket.on("connect_error", error => {
-      console.error("Socket.IO connection error:", error);
+      //console.error("Socket.IO connection error:", error);
     });
 
     socket.on("disconnect", () => {
-      console.log("Socket.IO connection closed:", socket.id);
+      //console.log("Socket.IO connection closed:", socket.id);
     });
 
     return () => {
-      console.log("Чистим слушателей события для WebSocket...");
+      //console.log("Чистим слушателей события для WebSocket...");
       socket.off("connect");
       socket.off(`roleUpdate:${user.id}`);
       socket.off("connect_error");
--- app/hooks/useTokenRefresh.ts
+++ app/hooks/useTokenRefresh.ts
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
 
-import axiosInstance from "@/libs/axios";
-
 import { useAuthStore } from "@/store/useAuthStore";
 
 export function useTokenRefresh() {
@@ -12,25 +10,25 @@ export function useTokenRefresh() {
     const interval = setInterval(
       async () => {
         try {
-          console.log("Попытка обновления токена...");
-          const response = await axiosInstance.post("/auth/login/access-token");
-          console.log("Токен обновлён:", response.data);
+          //console.log("Попытка обновления токена...");
+          //const response = await axiosInstance.post("/auth/login/access-token");
+          //console.log("Токен обновлён:", response.data);
           setRetryCount(0); // Сбрасываем счетчик после успешного обновления
         } catch (error) {
-          console.error("Ошибка при обновлении токена:", error);
+          /*  console.error("Ошибка при обновлении токена:", error); */
 
           if (retryCount >= 3) {
-            console.log(
+            /*  console.log(
               "Превышено количество попыток обновления токена, выполняем логаут..."
-            );
+            ); */
             logout();
           } else {
-            console.log("Повторная попытка обновления токена...");
+            /*  console.log("Повторная попытка обновления токена..."); */
             setRetryCount(prev => prev + 1);
           }
         }
       },
-      1 * 60 * 1000
+      10 * 60 * 1000
     );
 
     return () => clearInterval(interval);
