9ac37d41e04e1d3f54afdaef5e8a39903b5bdeff
AndrewSkrynnik <skrinnik306603@gmail.com>
2025-08-09 19:37:09 +0500

    fix

--- app/(main)/info/contacts/page.tsx
+++ app/(main)/info/contacts/page.tsx
@@ -2,6 +2,9 @@ import { Metadata } from "next";
 
 import { ContactsTemplate } from "@/features/info/components/ContactsTemplate";
 
+export const dynamic = "force-static"; // без динамики
+export const revalidate = 3600; // чтобы был SSG (можно больше/меньше)
+
 export const metadata: Metadata = {
   title: "Контакты | rotazap.ru",
   description:
